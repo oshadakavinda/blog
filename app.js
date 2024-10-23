6931b9d188ad47eab8c21ef07d3fb3da7ed328f8
oshadakavinda <oshadakavinda2@gmail.com>
2024-10-23 14:14:04 +0530

    modified:   app.js 	new file:   server/routes/main.js

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static('public'));
+//Templating Engine
+app.use(expressLayout);
+app.set('layout','/layouts/main');
+app.set('view engine','ejs');
+
+app.use('/',require('./server/routes/main'));
+
+// Correcting the route to use '/'
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
