d3d71f5b6a4ba5aa2dc2e8bd2d072feaf9acb8d5
oshadakavinda <oshadakavinda2@gmail.com>
2024-12-11 07:09:15 +0530

    modified:   public/css/style.css 	modified:   server/routes/main.js 	modified:   views/index.ejs

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,14 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post')
 
-router.get('',(req,res) => {
+router.get('',async (req,res) => {
 
     const locals = {
         title: "NodeJs Blog",
         description: "Simple Blog created with NodeJs, Express & MongoDb."
     }
 
-    res.render('index',locals);
+    let perPage =5;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{$sort:{createsAt: -1}}])
+    .skip(perPage*page - perPage)
+    .limit(perPage)
+    .exec();
+
+    // Count is deprecated - please use countDocuments
+    // const count = await Post.count();
+    const count = await Post.countDocuments({});
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    try {
+        const data = await Post.find();
+        res.render('index',{locals,data});
+    } catch (error) {
+        console.log(error);
+    }
+
+    res.render('index', { 
+        locals,
+        data,
+        current: page,
+        nextPage: hasNextPage ? nextPage : null,
+        currentRoute: '/'
+      });
 });
 
 
@@ -16,4 +44,7 @@ router.get('',(req,res) => {
 router.get('/about',(req,res) => {
     res.render('about');
 });
+
+
+
 module.exports = router;
