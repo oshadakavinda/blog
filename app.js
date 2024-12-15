989e9784d2d12d6e1faaa348bffe73662413ad44
oshadakavinda <oshadakavinda2@gmail.com>
2024-12-15 18:26:04 +0530

    new file:   server/helpers/routeHelpers.js     new file:   views/admin/add-post.ejs 	modified:   views/admin/dashboard.ejs 	new file:   views/admin/edit-post.ejs

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 require('dotenv').config();
+
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
 const connectDB = require('./server/config/db');
-const session = require('express-session');
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+  
 // Connect to DB
 connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 
 app.use(session({
   secret: 'keyboard cat',
@@ -24,20 +28,23 @@ app.use(session({
   store: MongoStore.create({
     mongoUrl: process.env.MONGODB_URI
   }),
+  //cookie: { maxAge: new Date ( Date.now() + (3600000) ) } 
 }));
-// Middleware for static files
+
 app.use(express.static('public'));
 
-// Templating engine
+// Templating Engine
 app.use(expressLayout);
-app.set('layout', 'layouts/main'); // Set default layout
-app.set('view engine', 'ejs'); // Set EJS as the view engine
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
-// Routes
+
+app.locals.isActiveRoute = isActiveRoute; 
+
+
 app.use('/', require('./server/routes/main'));
 app.use('/', require('./server/routes/admin'));
 
-// Start the server
-app.listen(PORT, () => {
+app.listen(PORT, ()=> {
   console.log(`App listening on port ${PORT}`);
 });
